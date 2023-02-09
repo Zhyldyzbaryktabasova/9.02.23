caa832a20efcf2bf2d0f9b11f27a4aa54e20a378
Zhyldyzbaryktabasova <zhyldyz.baryktabasovach@gmail.com>
2023-02-09 12:14:54 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// function nameIs () {
+//   console.log("Jyldyz");
+// }
+
+// nameIs ();
+// nameIs ();
+
+
+// function name0Republic(name1,name2) {
+//  console.log(num1 + num2)
+// }
+// name0Republic(5,5);
+// name0Republic(10,10);
+
+
+
+
+
+// let = 5;
+// let = 5;
+// function isEven(a) {
+
+//   if (a%2 ===1) {
+//     console.log("is not honest ")
+//   }
+//   else {
+//     console.log("is honest")
+//   }
+// }
+
+
+function countup(a, b) {
+  let ot = a;
+  while (ot <= b) {
+    console.log(ot)
+    ot++;
+  }
+}
+countup(5,10)
+countup(-10,10)
+
+
+function countdown(a, b){
+  let ot = a;
+  while (ot >= b) {
+    console.log(ot)
+    ot--;
+  }
+}
+countdown(10, 5);
+countdown(10, -10)
